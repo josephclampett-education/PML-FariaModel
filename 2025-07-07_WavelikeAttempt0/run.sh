e66f77c991171452e08d380846ff985fad82537e
josephclampett-education <111520419+josephclampett-education@users.noreply.github.com>
2025-07-10 09:30:24 -0400

    Update run.sh

--- 2025-07-07_WavelikeAttempt0/run.sh
+++ 2025-07-07_WavelikeAttempt0/run.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=64g
 #SBATCH --mail-type=begin,end,fail
-#SBATCH --mail-user=jclampett@unc.unc.edu
+#SBATCH --mail-user=jclampett@unc.edu
 #SBATCH -t 07-00:00:00
 
 module load matlab/2023b
