abecf7bad49b5229c055a7bb75727d2c5a6f49fc
josephclampett-education <111520419+josephclampett-education@users.noreply.github.com>
2025-07-17 09:38:36 -0400

    Preparing 01

--- 2025-07-15/00/run.sh
+++ 2025-07-15/00/run.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32g
+#SBATCH --mem=40g
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --mail-user=jclampett@unc.edu
 #SBATCH -t 07-00:00:00
