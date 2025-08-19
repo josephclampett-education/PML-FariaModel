b6dedb31bc013223505aa06e422bc6ff15667a82
Xinyun Liu <xinyunliu.nju@gmail.com>
2025-08-19 12:45:11 -0400

    Set up 43 to use wavefield video plotting

--- 2025-08-18/43/postprocess.m
+++ 2025-08-18/43/postprocess.m
@@ -28,6 +28,8 @@ parfor i = 1:threadCount
 
     x_data = [];
     y_data = [];
+    x_runs = cell(numel(files), 1);
+    y_runs = cell(numel(files), 1);
     for j = 1:numel(files)
         filePath = fullfile(files(j).folder, files(j).name);
         loadP = load(filePath);
@@ -35,34 +37,35 @@ parfor i = 1:threadCount
 
         x_data = [x_data; p.x_data];
         y_data = [y_data; p.y_data];
+
+        x_runs{j} = p.x_data;
+        y_runs{j} = p.y_data;
     end
-    p.x_data = x_data;
-    p.y_data = y_data;
 
     close all
 
     %% Setup
     xs = [];
     ys = [];
-    for j = 1:size(p.x_data, 2)
-        xs = [xs; p.x_data(:, j)];
-        ys = [ys; p.y_data(:, j)];
+    for j = 1:size(x_data, 2)
+        xs = [xs; x_data(:, j)];
+        ys = [ys; y_data(:, j)];
     end
 
     bounds = [-(p.Rc + 1), (p.Rc + 1)];
     
-    %% Figure 1
-    figure(1)
+    %% HISTOGRAM
+    figure
     cellcount = 180;
     
-    xEdges = linspace(-p.Lx/2, p.Lx/2, cellcount);
-    yEdges = linspace(-p.Ly/2, p.Ly/2, cellcount);
+    xEdges = linspace(-p.Lx/2, p.Lx/2, cellcount + 1);
+    yEdges = linspace(-p.Ly/2, p.Ly/2, cellcount + 1);
     [bins, xEdges, yEdges] = histcounts2(xs, ys, xEdges, yEdges, Normalization="probability");
-    gridX = meshgrid(xEdges(1:end-1));
-    gridY = meshgrid(yEdges(1:end-1))';
+
+    axisValuesX = linspace(-p.Lx/2, p.Lx/2, cellcount);
+    axisValuesY = linspace(-p.Ly/2, p.Ly/2, cellcount);
     hold on
-    pcolorHandle = pcolor(gridX, gridY, bins);
-    set(pcolorHandle, 'EdgeColor', 'none');
+    imagesc(axisValuesX, axisValuesY, bins');
     viscircles([0, 0], [p.Rc], 'LineWidth', 0.2, 'LineStyle','--');
     hold off
     
@@ -73,10 +76,51 @@ parfor i = 1:threadCount
     %clim([0.1, 1.7] * 10e-5)
 
     exportgraphics(gca, pathId + "_histogram.png");
-
     
-    %% Figure 2
-    figure(2)
+    %% VELOCITY HISTOGRAM
+    figure
+    bincount = 80;
+
+    velEdges = linspace(0, 20, bincount + 1);
+    velBins = zeros(1, bincount);
+    for j = 1:numel(x_runs)
+        xs_pl = x_runs{j};
+        ys_pl = y_runs{j};
+    
+        vx_plpf = diff(xs_pl, 1, 1);
+        vy_plpf = diff(ys_pl, 1, 1);
+    
+        vs_mmpf = sqrt(vx_plpf.^2 + vy_plpf.^2) * p.lambdaF * 1000;
+        vs_mmps = vs_mmpf * 1/p.TF;
+    
+        counts = histcounts(vs_mmps, velEdges);
+    
+        velBins = velBins + counts;
+    end
+
+    histogram(BinEdges = velEdges, BinCounts = velBins)
+    xlim([0 20])
+
+    exportgraphics(gca, pathId + "_velocity_histogram.png");
+    
+    %% VELOCITY HISTOGRAM
+    figure
+
+    xs_pl = p.x_data;
+    ys_pl = p.y_data;
+
+    vx_plpf = diff(xs_pl, 1, 1);
+    vy_plpf = diff(ys_pl, 1, 1);
+
+    vs_mmpf = sqrt(vx_plpf.^2 + vy_plpf.^2) * p.lambdaF * 1000;
+    vs_mmps = vs_mmpf * 1/p.TF;
+
+    plot(vs_mmps);
+
+    exportgraphics(gca, pathId + "_velocity_plot.png");
+    
+    %% RADIAL HISTOGRAM
+    figure
     bincount = 80;
     
     r_data = sqrt(xs.^2 + ys.^2);
@@ -97,11 +141,11 @@ parfor i = 1:threadCount
 
     exportgraphics(gca, pathId + "_radial_histogram.png");
 
-    %% Figure 3
-    figure(3)
+    %% TRAJECTORY
+    figure
     hold on
     for j = 1:size(p.x_data, 2)
-        plot(p.x_data(:, j), p.y_data(:, j));
+        plot(x_data(:, j), y_data(:, j));
     end
     viscircles([0, 0], [p.Rc], 'LineWidth', 0.2, 'LineStyle','--');
     hold off
@@ -112,8 +156,8 @@ parfor i = 1:threadCount
 
     exportgraphics(gca, pathId + "_trajectory.png");
 
-    %% Figure 4
-    figure(4)
+    %% WAVEFIELD
+    figure
     hold on
     wavefield = p.eta_data(:, :, end);
     contourf(p.xx, p.yy, wavefield, 50, "EdgeColor", "none");
@@ -126,4 +170,41 @@ parfor i = 1:threadCount
     ylim(bounds)
 
     exportgraphics(gca, pathId + "_wavefield.png");
+
+    %% WAVEFIELD (VIDEO)
+    v = VideoWriter(pathId + "_wavefield.avi", 'Motion JPEG AVI');
+    v.FrameRate = 1/p.TF;
+    v.Quality = 95;
+    open(v);
+
+    frameCount = size(p.eta_data, 3);
+    figureHandle = figure;
+    for j = 1:frameCount
+        % Wavefield
+        wavefield = p.eta_data(:, :, j);
+        contourf(p.xx, p.yy, wavefield, 50, "EdgeColor", "none");
+
+        hold on
+        % Droplet positions
+        absoluteTime = p.nimpacts - p.n_save_wave + j;
+        dropletPositions = zeros(p.n_drops, 2);
+        for k = 1:size(p.x_data, 2)
+            dropletPositions = [p.x_data(absoluteTime, k), p.y_data(absoluteTime, k)];
+        end
+        viscircles(dropletPositions, p.drop_radius / p.lambdaF * ones(1, p.n_drops));
+
+        % Corral
+        viscircles([0, 0], [p.Rc], 'LineWidth', 0.2, 'LineStyle','--');
+        hold off
+
+        axis square
+        xlim(bounds)
+        ylim(bounds)
+        title(sprintf("Wavefield %d / %d", j, frameCount));
+
+        frame = getframe(figureHandle);
+        writeVideo(v, frame);
+    end
+
+    close(v);
 end
