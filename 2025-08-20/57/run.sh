ab1ba9c4a337cfa262b806ac379cfadfa807aaa4
Xinyun Liu <xinyunliu.nju@gmail.com>
2025-08-21 11:47:03 -0400

    Changed 55 and 57 run

--- 2025-08-20/57/run.sh
+++ 2025-08-20/57/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32g
-#SBATCH --array=1-1
+#SBATCH --array=1-10
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --mail-user=jclampett@unc.edu
 #SBATCH -t 07-00:00:00
