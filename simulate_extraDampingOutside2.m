e3b27e1bc7b6d636d7ca7c1f181c42c0ec6df6ff
Joseph Clampett <jclampett@unc.edu>
2025-11-06 16:24:34 -0500

    Set up 88

---
+++ simulate_extraDampingOutside2.m
@@ -0,0 +1,71 @@
+function p = simulate_extraDampingOutside2(p)
+
+% Set Initial Conditions
+t   = p.theta/(4*pi);
+phi = p.phi0;
+eta = p.eta0; 
+xi  = p.xi; 
+yi  = p.yi; 
+ui  = p.ui; 
+vi  = p.vi; 
+
+% Create Variables to Store Data
+x_data   = zeros(p.nimpacts,p.n_drops); 
+y_data   = zeros(p.nimpacts,p.n_drops); 
+t_data   = zeros(p.nimpacts,1);
+eta_data = zeros(p.Nx,p.Ny,p.n_save_wave); 
+
+% Fourier Transform of Wave Initial Condition
+phi_hat = fft2(phi);               
+eta_hat = fft2(eta);
+
+for n = 1:p.nimpacts
+    
+    % Wave Field at Current Time
+    eta = real(ifft2(eta_hat));  
+    phi = real(ifft2(phi_hat));
+
+    % Adding Extra Damping Outside Rc with 1 Damping Wavelength
+    r = sqrt(p.xx.^2 + p.yy.^2);
+    eta(r > p.Rc) = eta(r > p.Rc) .* exp(-(r(r > p.Rc) - p.Rc)/0.2);
+    phi(r > p.Rc) = phi(r > p.Rc) .* exp(-(r(r > p.Rc) - p.Rc)/0.2);
+    
+    phi_hat = fft2(phi);               
+    eta_hat = fft2(eta);
+
+    % Ensure below Faraday threshold
+    eta_max = max(max(abs(eta)));
+    if eta_max > 1
+        fprintf("%s: eta_max %f > 1 during simulation.\n", datetime, eta_max);
+        break
+    end
+
+    % Store Data
+    x_data(n,:)     = xi; 
+    y_data(n,:)     = yi; 
+    t_data(n)       = t;
+    
+    % Save wavefield for the last 10 periods
+    if n > p.nimpacts - p.n_save_wave
+        eta_data(:,:, n - p.nimpacts + p.n_save_wave) = eta;
+    end
+    
+    % Drop Impact
+    [ui, vi, phi_hat] = drop_impact(xi,yi, ui, vi, phi_hat, eta_hat, p);
+    
+    % Evolve Drops Between Impacts
+    [xi, yi, ui, vi] = evolve_drops(xi, yi, ui, vi, p);
+
+    % Evolve Wave Between Impacts
+    [phi_hat, eta_hat] = evolve_wave(phi_hat, eta_hat, t, p.Gam, p); 
+    
+    % Update Time
+    t = t+p.impact_interval;
+
+end
+
+% Put Data in Output Structure
+p.x_data   = x_data; 
+p.y_data   = y_data; 
+p.t_data   = t_data;
+p.eta_data = eta_data; 
